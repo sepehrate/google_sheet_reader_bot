f2959b43aa87b0fc22bb44da74d993b7682fdaf5
sepehrate <arefmanesh@gmail.com>
2019-08-13 17:13:31 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
- 'TelegramProductionURL': 'https://satrmedia.ir/', // do not forget trailing "/" !!
+ 'TelegramProductionURL': 'https://gamestoon.herokuapp.com/', // do not forget trailing "/" !!
  'TelegramToken': '962227030:AAHt6ZpuLnrJo8JOirqkmH3t0Y4grw84OwU', // Telegram token
  'googleSheetKey': '1fq3lze-uCnY8X-8BnZjKRrtrilJU65-cq6x6cf96DsY', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
  "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
