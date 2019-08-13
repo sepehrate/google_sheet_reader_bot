bf15c48f18c2f42672dc57747f2efcbaf167ef07
sepehrate <arefmanesh@gmail.com>
2019-08-13 17:11:00 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
  'TelegramProductionURL': 'https://satrmedia.ir/', // do not forget trailing "/" !!
  'TelegramToken': '962227030:AAHt6ZpuLnrJo8JOirqkmH3t0Y4grw84OwU', // Telegram token
- 'googleSheetKey': '13H-qqA427mPnphFG3agU735f9N5mdM3odhRynKzXnI8', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ 'googleSheetKey': '1fq3lze-uCnY8X-8BnZjKRrtrilJU65-cq6x6cf96DsY', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
  "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
 }
