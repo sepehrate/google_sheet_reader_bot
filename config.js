48e2c3227c0a0ef7a26a88a6e6f145ef56406281
sepehrate <arefmanesh@gmail.com>
2020-07-20 16:25:55 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
- 'TelegramProductionURL': 'https://gamestoon.herokuapp.com/', // do not forget trailing "/" !!
- 'TelegramToken': '733511441:AAHtAfZsGYEJTdjn37ul9pFiOl-2MgOWs9w', // Telegram token
- 'googleSheetKey': '1fq3lze-uCnY8X-8BnZjKRrtrilJU65-cq6x6cf96DsY', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
- "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
+ 'TelegramProductionURL': 'https://ecoiran.herokuapp.com/', // do not forget trailing "/" !!
+ 'TelegramToken': '1223721962:AAHrPk4vq7MLnuiIDL5LL0CH9pTWrIxPblI', // Telegram token
+ 'googleSheetKey': '1sT2IhmaoWeJCdG8ikLZut9Iw1C7LQQsdU5V1RBIGg2c', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ "confTimeZone": "Asia/Tehran" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
 }
